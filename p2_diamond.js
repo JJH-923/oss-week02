79d29119735e03b5157a7dc8ddde826ce6706781
Jinhee Jeong <jinnn923@kw.ac.kr>
2026-09-10 11:03:06 +0900

    p2: diamond

--- p2_diamond.js
+++ p2_diamond.js
@@ -10,6 +10,31 @@
 //   *
 
 const n = Number(process.argv[2] ?? 5);
+const mid = Math.floor(n / 2);
+
+for (let i = 0; i < n; i++) {
+    let line = "";
+
+    if (i <= mid) {
+        for (let j = 0; j < mid - i; j++) {
+            line += " ";
+        }
+
+        for (let j = 0; j < 2 * i + 1; j++) {
+            line += "*";
+        }
+    } else {
+        for (let j = 0; j < i - mid; j++) {
+            line += " ";
+        }
+
+        for (let j = 0; j < 2 * (n - i) - 1; j++) {
+            line += "*";
+        }
+    }
+
+    console.log(line);
+}
 
 // TODO: print the diamond with for / if and console.log.
 // Hint: for row i, count the spaces and the stars separately.
